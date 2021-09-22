c25b63e95be68e18daa0ffa9e7cb99e49e1500ff
dongliu <dongliu@kuaishou.com>
2021-09-22 16:59:38 +0800

    temporarily remove protocol with properties

--- appkit/animatable_property_container.h
+++
@@ -1,8 +0,0 @@
-#import <stdbool.h>
-#import <stdint.h>
-#import <stdlib.h>
-#import <utils.h>
-#import <Foundation/NSGeometry.h>
-#import <Foundation/NSRange.h>
-
-void* WrapAnimatablePropertyContainer(uintptr_t goID);
--- appkit/dragging_info.h
+++
@@ -1,8 +0,0 @@
-#import <stdbool.h>
-#import <stdint.h>
-#import <stdlib.h>
-#import <utils.h>
-#import <Foundation/NSGeometry.h>
-#import <Foundation/NSRange.h>
-
-void* WrapDraggingInfo(uintptr_t goID);
